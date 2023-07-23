5f1eaab195959cba10e10b0857d3bc559d14754f
Rohit <www.rahulveeru12345@gmail.com>
2023-07-23 13:59:51 +0530

    final commit

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
- h
+ var image=document.getElementById("image");
+ function imgchange1(){
+    image.src="maldives.jpg";
+ }
+ function imgchange2(){
+    image.src="hawaii.jpg";
+ }
+ function imgchange3(){
+    image.src="japan.jpg";
+ }
+ function btndisplay(){
+    var obj=document.getElementById("btnbox");
+    var obj2=document.getElementById("button");
+     var styles={
+        display:'flex'
+     };
+     Object.assign(obj.style,styles);
+     var styles1={
+        display:'none'
+     };
+     Object.assign(obj2.style,styles1);
+
+    }
+
+    
+ 
